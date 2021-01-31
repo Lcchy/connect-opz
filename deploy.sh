d9b10e9c777e8d54b8c771291368eff03a9b26ee
Lcchy <52164716+Lcchy@users.noreply.github.com>
2021-01-31 14:56:46 +0100

    Working setup for Digitakt with Fates

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo cp 99-digitakt.rules /etc/udev/rules.d/
+sudo cp digitakt-jack-audio.service /etc/systemd/system/
+
+sudo systemctl daemon-reload
+sudo udevadm control --reload-rules && udevadm trigger
